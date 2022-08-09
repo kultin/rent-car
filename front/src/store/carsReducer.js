bb3dc0357f6641cfdded9563fd5a68b3d99cb04b
Olga <olga.onoprienko2021@gmail.com>
2022-08-09 17:16:07 +0300

    pagination, filters

--- front/src/store/carsReducer.js
+++ front/src/store/carsReducer.js
@@ -1,14 +1,20 @@
 import ACTypes from "./types"
+import { initCars } from "./initCars"
 
-const initCars = [
-  {id:1, brand: "Volkswagen", model: "Polo", body: "sedan", year:2018, engine: "1,6 ", gear: "Автомат", power: "110", seats: "ткань", photo:"", price: 2500},
-  {id:2, brand: "Skoda", model: "Rapid", body: "sedan", year:2020, engine: "1,6 ", gear: "Автомат", power: "110", seats: "ткань", photo:"", price: 2500},
-  {id:3, brand: "Nissan", model: "Quashqai", body: "sedan", year:2020, engine: "1,6 ", gear: "Автомат", power: "144",seats: "экокожа", photo:"", price: 3500},
-  {id:4, brand: "Renault", model: "Arkana", body: "crossover", year:2022, engine: "2 ", gear: "Автомат", power: "184",seats: "кожа", photo:"", price: 3500},
-  {id:5, brand: "Renault", model: "Kaptur", body: "crossover", year:2021, engine: "2 ", gear: "Автомат", power: "150",seats: "кожа",photo:"", price: 2900}
-]
+
+// const initCars = [
+//   {id:1, brand: "Volkswagen", model: "Polo", body: "sedan", year:2018, engine: "1,6 ", gear: "Автомат", power: "110", seats: "ткань", photo:"", price: 2500, class: "econom"},
+//   {id:2, brand: "Volkswagen", model: "Passat B8", body: "sedan", year:2018, engine: "1,6 ", gear: "Автомат", power: "110", seats: "ткань", photo:"", price: 4200, class: "business"},
+//   {id:3, brand: "Skoda", model: "Rapid", body: "sedan", year:2020, engine: "1,6 ", gear: "Автомат", power: "110", seats: "ткань", photo:"", price: 2500, class: "econom"},
+//   {id:4, brand: "Nissan", model: "Quashqai", body: "sedan", year:2020, engine: "1,6 ", gear: "Автомат", power: "144",seats: "экокожа", photo:"", price: 3500, class: "comfort"},
+//   {id:5, brand: "Renault", model: "Arkana", body: "crossover", year:2022, engine: "2 ", gear: "Автомат", power: "184",seats: "кожа", photo:"", price: 3500, class: "econom"},
+//   {id:6, brand: "Renault", model: "Kaptur", body: "crossover", year:2021, engine: "2 ", gear: "Автомат", power: "150",seats: "кожа",photo:"", price: 2900, class: "econom"},
+//   {id:7, brand: "Toyota", model: "Camry V55", body: "crossover", year:2021, engine: "2 ", gear: "Автомат", power: "150",seats: "кожа",photo:"", price: 8500, class: "business"},
+//   {id:8, brand: "Audi", model: "A3", body: "crossover", year:2019, engine: "2 ", gear: "Автомат", power: "150",seats: "кожа",photo:"", price: 3500, class: "comfort"}
+// ]
 
 const initialState = {
+  
   cars: initCars,
   filteredCars: [],
   filter: false
@@ -22,16 +28,42 @@ export const carsReducer = (state = initialState, action) => {
       
     //   return {...state, cars: action.payload};
 
-    case ACTypes.SET_FILTER:
-      console.log(action.payload);
-      return {...state, filter: action.payload};
 
-      case ACTypes.FILTER:      
+      case ACTypes.BRAND_FILTER:      
         const brand=action.payload.brand;
+        if (brand==="все марки")  return {...state, filter:true, filteredCars: state.cars};
         const filteredCars=state.cars.filter((car)=>car.brand===brand);
         console.log(filteredCars);
-        return {...state, filteredCars: filteredCars};
-  
+        return {...state, filter:true, filteredCars: filteredCars};
+
+
+      case ACTypes.BODY_FILTER:  
+      const body=action.payload.body;
+      const filteredByBody=state.cars.filter((car)=>car.body===body);
+      console.log(filteredByBody);
+      return {...state, filter:true, filteredCars: filteredByBody};
+
+      case ACTypes.CLASS_FILTER:  
+      
+      const carClass=action.payload.class;
+      console.log(carClass)
+      const filteredByClass=state.cars.filter((car)=>car.class===carClass);
+      console.log(filteredByClass);
+      return {...state, filter:true, filteredCars: filteredByClass};
+
+
+      case ACTypes.PRICE_FILTER:      
+        const price=action.payload.price;
+        console.log(price)
+        if (price==="по возрастанию")  {
+          const filteredByPrice=state.cars.sort((a,b)=>a.price -b.price);
+          return {...state, filter:true, filteredCars: filteredByPrice }
+        } else {
+          const filteredByPrice=state.cars.sort((a,b)=>b.price -a.price);
+          return {...state, filter:true, filteredCars:filteredByPrice }
+        }
+        
+
     default:
       return state;  
   }
