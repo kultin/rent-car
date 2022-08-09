f8bae9a0a15b4cf0c8d2d3f0ab3b3fd3f8b40a8e
Nikolai Trunin <koljatru@yandex.ru>
2022-08-09 12:22:16 +0300

    slider-v2

--- front/src/App.js
+++ front/src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import LogIn from './components/LogIn';
 import LogUp from './components/LogUp';
 import Layout from './components/Layout/Layout';
-import Home from './components/Private/Private';
+import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound'
 import Private from './components/Private/Private'
 
@@ -15,7 +15,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="logup" element={<LogUp />} />
         <Route path="login" element={<LogIn />} />
-        <Route path="private" element={<Private />}></Route>
+        <Route path="private" element={<Private />}/>
         {/* <Route path="cars" element={<Cars/>}></Route> */}
       </Route>
       <Route path='*' element={<NotFound />} />
