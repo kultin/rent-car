ac806dd4d3e672c5c1381ebf11a9debdbe0afddf
Olga <olga.onoprienko2021@gmail.com>
2022-08-10 11:53:32 +0300

    filters, pagination commit2

--- front/src/components/Calendar/Calendar.jsx
+++ front/src/components/Calendar/Calendar.jsx
@@ -4,12 +4,15 @@ import React from 'react'
 import 'react-calendar/dist/Calendar.css';
 
 export default function CalendarComponent() {
-  const [calValue, setCalValue] =useState(new Date())
+  const [date, setDate] =useState(new Date())
+  console.log(date.toString())
   return (
+   
     <div>
       Calendar
-      <Calendar value ={calValue} onChange={setCalValue}
+      <Calendar value ={date} onChange={setDate}
       next2Label={null} prev2Label={null}/>
+      <button></button>
       </div>
   )
 }
