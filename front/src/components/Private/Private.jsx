735020f2344de87b415865f719e002cbc5fe9842
Aleksandr <tsupko_a@mail.ru>
2022-08-16 17:23:34 +0400

    Отображение ЛК и редактирование профиля

--- front/src/components/Private/Private.jsx
+++ front/src/components/Private/Private.jsx
@@ -3,8 +3,8 @@ import './private.modules.scss';
 import Privateinfo from './Privatecomponents/Privateinfo'
 import Privatebookings from './Privatecomponents/Privatebookings'
 import Privatefavorites from './Privatecomponents/Privatefavorites'
-import { getBookingsThunk } from '../../store/userActions'
-import { useDispatch } from 'react-redux';
+import { getBookingsThunk, getUserThunk } from '../../store/userActions'
+import { useDispatch, useSelector } from 'react-redux';
 import AddCArForm from "./Privatecomponents/AddCarForm";
 import PrivateCars from "./Privatecomponents/PrivateCars";
 
@@ -13,23 +13,33 @@ export default function Private() {
 
   const dispatch = useDispatch()
 
+  const { user } = useSelector((store) => (store.user))
+
   React.useEffect(() => {
     dispatch(getBookingsThunk())
   }, [])
 
-  return (
-    <>
-      <div className="container">
-        <div className="user__box">
-          <Privateinfo />
-          <div className="user__box-value">
-            <Privatebookings />
-            <Privatefavorites />
-            <AddCArForm />
-            <PrivateCars />
-          </div>
+  return ((user.role == 'lessor') ? (
+    <div className="container">
+      <div className="user__box">
+        <Privateinfo />
+        <div className="user__box-value">
+          <Privatebookings title="Мои заказы" />
+          <AddCArForm />
+          <PrivateCars />
         </div>
       </div>
-    </>
+    </div>
+  ) : (
+    <div className="container">
+      <div className="user__box">
+        <Privateinfo />
+        <div className="user__box-value">
+          <Privatebookings title="Мои бронирования" />
+          <Privatefavorites />
+        </div>
+      </div>
+    </div>
+  )
   )
 }
